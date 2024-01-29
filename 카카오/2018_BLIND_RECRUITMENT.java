78977c95ea776850a54d194dd9b8ce13bbdef495
khabh <khabh@naver.com>
2024-01-29 15:59:15 +0900

    KAKAO(2019_BLIND_RECRUITMENT) 문제 풀이

--- 카카오/2018_BLIND_RECRUITMENT.java
+++ 카카오/2018_BLIND_RECRUITMENT.java
@@ -208,3 +208,205 @@ class Solution {
         return Arrays.stream(scores).sum();
     }
 }
+
+// 캐시
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        LinkedList<String> cache = new LinkedList<>();
+        Set<String> exists = new HashSet<>();
+        int total = 0;
+        for (String c : cities) {
+            String city = c.toLowerCase();
+            if (exists.contains(city)) {
+                total += 1;
+                cache.remove(city);
+            } else {
+                total += 5;
+                if (cache.size() == cacheSize) {
+                    String removed = cache.removeFirst();
+                    exists.remove(removed);
+                }
+            }
+            cache.add(city);
+            exists.add(city);
+        }
+        return total;
+    }
+}
+
+
+// 뉴스 클러스터링
+import java.util.*;
+
+class Solution {
+    
+    private static final int PREFIX = 65536;
+    
+    public int solution(String str1, String str2) {
+        Map<String, Integer> first = convert(str1);
+        Map<String, Integer> second = convert(str2);
+        Set<String> inter = new HashSet<>(first.keySet());
+        inter.retainAll(second.keySet());
+        int interCount = 0;
+        for (String s : inter) {
+            interCount += (Math.min(first.get(s), second.get(s)));
+        }
+        int totalCount = countAll(first) + countAll(second) - interCount;
+        if (totalCount == 0) {
+            return PREFIX;
+        }
+        return (int)(((double)interCount / totalCount) * PREFIX);
+    }
+    
+    private int countAll(Map<String, Integer> map) {
+        int result = 0;
+        for (String s : map.keySet()) {
+            result += map.get(s);
+        }
+        return result;
+    }
+
+    private Map<String, Integer> convert(String str) {
+        Map<String, Integer> map = new HashMap<>();
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length() - 1; i++) {
+            String cur = str.substring(i, i + 2);
+            if (!cur.matches("[a-z]{2}")) {
+                continue;
+            }
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+        }
+        return map;
+    }
+}
+
+// 프렌즈4블록
+import java.util.*;
+
+class Solution {
+    
+    private static final int[] dx = {1, 0, 1, 0};
+    private static final int[] dy = {0, 1, 1, 0};
+    
+    private char[][] graph;
+
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        graph = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            graph[i] = board[i].toCharArray();
+        }
+        while (true) {
+            List<Node> nodes = new ArrayList<>();
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n - 1; j++) {
+                    if (graph[i][j] != ' ' && canRemove(i, j)) {
+                        nodes.add(new Node(i, j));
+                    }
+                }
+            }
+            if (nodes.isEmpty()) {
+                break;
+            }
+            for (Node node : nodes) {
+                answer += remove(node.x, node.y);
+            }
+            for (int j = 0; j < n; j++) {
+                for (int i = m - 1; i >= 0; i--) {
+                    if (graph[i][j] == ' ')
+                        continue;
+                    int index = i;
+                    while (index + 1 < m && graph[index + 1][j] == ' ') {
+                        index++;
+                    }
+                    if (index == i)
+                        continue;
+                    graph[index][j] = graph[i][j];
+                    graph[i][j] = ' ';
+                }
+            }
+        }
+        return answer;
+    }
+    
+    private int remove(int x, int y) {
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (graph[nx][ny] == ' ') {
+                continue;
+            }
+            count++;
+            graph[nx][ny] = ' ';
+        }
+        return count;
+    }
+    
+    private boolean canRemove(int x, int y) {
+        char cur = graph[x][y];
+        for (int i = 0; i < 3; i++) {
+            if (graph[x + dx[i]][y + dy[i]] != cur) {
+                return false;
+            }
+        }
+        return true;
+    }
+        
+    class Node {
+        int x;
+        int y;
+        
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+// 추석 트래픽
+class Solution {
+    int[] starts = new int[24 * 60 * 60 * 1000];
+    int[] ends = new int[24 * 60 * 60 * 1000];
+    
+    public int solution(String[] lines) {
+        for (String line : lines) {
+            String[] split = line.split(" ");
+            int endTime = getTime(split[1]);
+            addTime(endTime, split[2]);
+        }
+        int count = 0;
+        for (int i = 0; i < 1000; i++) {
+            count += starts[i];
+        }
+        int answer = count;
+        for (int i = 1000; i < starts.length; i++) {
+            count += starts[i];
+            count -= ends[i - 1000];
+            answer = Math.max(answer, count);
+        }
+        return answer;
+    }
+    
+    private void addTime(int endTime, String s) {
+        int time = (int)(1000 * Double.parseDouble(s.split("s")[0]));
+        int start = Math.max(0, endTime - time + 1);
+        int end = Math.min(endTime, starts.length - 1);
+        starts[start]++;
+        ends[end]++;
+    }
+    
+    private int getTime(String input) {
+        String[] split = input.split(":");
+        int h = Integer.parseInt(split[0]) * 60 * 60 * 1000;
+        int m = Integer.parseInt(split[1]) * 60 * 1000;
+        int s = (int)(Double.parseDouble(split[2]) * 1000);
+
+        return h + m + s;
+    }
+}
