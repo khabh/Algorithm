0f2a7004ed45e8ba499534c599940c4618e40798
JUHA <84626225+khabh@users.noreply.github.com>
2024-04-04 09:26:46 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 300 ms, Memory: 21288 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+            dp[i] = numbers[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int currentValue = dp[i];
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[j] > numbers[i]) {
+                    dp[j] = Math.max(dp[j], currentValue + numbers[j]);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().orElse(0));
+    }
+}
