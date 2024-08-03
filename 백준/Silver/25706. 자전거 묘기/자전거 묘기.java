5380757e66ed7921da98a020cff37cca6375dbbe
JUHA <84626225+khabh@users.noreply.github.com>
2024-08-03 17:11:45 +0900

    [Silver III] Title: 자전거 묘기, Time: 1452 ms, Memory: 58476 KB -BaekjoonHub

---
+++ 백준/Silver/25706. 자전거 묘기/자전거 묘기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] dp = new int[n];
+        int[] heights = new int[n];
+        for (int i = 0; i < n; i++) {
+            heights[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.fill(dp, 1);
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = heights[i] + i < n - 1 ? dp[i + heights[i] + 1] + 1 : 1;
+        }
+        for (int num : dp) {
+            System.out.print(num + " ");
+        }
+    }
+}
