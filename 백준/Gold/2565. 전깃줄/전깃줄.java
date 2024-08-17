7284e28e2b3e1348974c0483e2cff96a93382a56
JUHA <84626225+khabh@users.noreply.github.com>
2024-08-17 11:15:05 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            lines.add(new Line(a, b));
+        }
+        Collections.sort(lines);
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (lines.get(i).end > lines.get(j).end) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.println(n - Arrays.stream(dp).max().getAsInt());
+    }
+
+    private static class Line implements Comparable<Line> {
+
+        int start;
+        int end;
+
+        public Line(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Line o) {
+            return start - o.start;
+        }
+    }
+}
