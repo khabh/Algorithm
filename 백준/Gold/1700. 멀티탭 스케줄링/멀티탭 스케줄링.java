7331793d7f970fca3a2c0efa135691015728793d
JUHA <84626225+khabh@users.noreply.github.com>
2024-09-08 12:20:30 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        List<Node> list = new ArrayList<>();
+        for (int i = 0; i <= k; i++) {
+            list.add(new Node(i));
+        }
+        int[] nodes = new int[k];
+        for (int i = 0; i < k; i++) {
+            int node = Integer.parseInt(st.nextToken());
+            nodes[i] = node;
+            list.get(node).add(i);
+        }
+        for (Node node : list) {
+            node.add(k + 1);
+        }
+        Set<Node> plugs = new HashSet<>();
+        int count = 0;
+        for (int i : nodes) {
+            Node node = list.get(i);
+            int first = node.pop();
+
+            if (plugs.contains(node)) {
+                continue;
+            }
+            if (plugs.size() < n) {
+                plugs.add(node);
+                continue;
+            }
+
+            count++;
+            Node removed = node;
+            for (Node plugged : plugs) {
+                if (plugged.q.getFirst() > first) {
+                    first = plugged.q.getFirst();
+                    removed = plugged;
+                }
+            }
+            plugs.remove(removed);
+            plugs.add(node);
+        }
+        System.out.println(count);
+    }
+
+    static class Node {
+        int num;
+        Deque<Integer> q = new LinkedList<>();
+
+        public Node(int num) {
+            this.num = num;
+        }
+
+        public void add(int order) {
+            q.add(order);
+        }
+
+        public int pop() {
+            return q.poll();
+        }
+    }
+}
