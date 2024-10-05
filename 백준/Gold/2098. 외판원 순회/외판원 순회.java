8d12d8dcf772b9b3e1085cd45ad710e41e15ddac
juha <84626225+khabh@users.noreply.github.com>
2024-10-05 11:49:06 +0900

    [Gold I] Title: 외판원 순회, Time: 780 ms, Memory: 21432 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -16,14 +16,12 @@ public class Main {
         }
 
         int max = 1 << n;
-        int[][][] dp = new int[n][n][max];
+        int[][] dp = new int[n][max];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
+            Arrays.fill(dp[i], -1);
         }
-        // 방문한 도시 수, 직전 방문 도시, 전체 방문 도시
-        dp[0][0][1 << 0] = 0;
+
+        dp[0][1 << 0] = 0;
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int cur = 1; cur < n; cur++) {
@@ -31,14 +29,14 @@ public class Main {
                         continue;
                     int curCost = costs[j][cur];
                     for (int k = 0; k < max; k++) {
-                        int prev = dp[i - 1][j][k];
+                        int prev = dp[j][k];
                         if ((k & (1 << cur)) != 0 || prev == -1) {
                             continue;
                         }
                         int temp = k | (1 << cur);
                         int cost = prev + curCost;
-                        if (dp[i][cur][temp] == -1 || dp[i][cur][temp] > cost) {
-                            dp[i][cur][temp] = cost;
+                        if (dp[cur][temp] == -1 || dp[cur][temp] > cost) {
+                            dp[cur][temp] = cost;
                         }
                     }
                 }
@@ -47,7 +45,7 @@ public class Main {
 
         int result = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            int cur = dp[n - 1][i][max - 1];
+            int cur = dp[i][max - 1];
             if (cur != -1 && costs[i][0] != 0) {
                 result = Math.min(result, cur + costs[i][0]);
             }
