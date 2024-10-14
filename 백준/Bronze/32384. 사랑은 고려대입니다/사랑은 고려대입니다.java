1712d3c275265f2c9c0e3a902251eed0e3d82a87
juha <84626225+khabh@users.noreply.github.com>
2024-10-14 15:14:45 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 164 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException { 
+        int n = new Scanner(System.in).nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("LoveisKoreaUniversity");
+        }
+    }
+}
