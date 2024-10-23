92b515d3bec432a439e3e5b4dce561276ce46315
juha <84626225+khabh@users.noreply.github.com>
2024-10-23 09:31:19 +0900

    [Gold V] Title: 결전의 금요일, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/25194. 결전의 금요일/결전의 금요일.java
+++ 백준/Gold/25194. 결전의 금요일/결전의 금요일.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int[] nums = Arrays.stream(br.readLine().split(" "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
         boolean[] dp = new boolean[7];
         dp[1] = true;
         
