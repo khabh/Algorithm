7101bb833f2efbe2d949d1ee66ea595ead2c6f26
juha <84626225+khabh@users.noreply.github.com>
2024-11-16 17:54:11 +0900

    [Gold II] Title: 사과와 바나나, Time: 784 ms, Memory: 221548 KB -BaekjoonHub

--- 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
+++ 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
@@ -3,14 +3,35 @@ import java.io.*;
 
 class Main {
 
+    private static int n, m;
+    private static int[][] a, total;
+
     public static void main(String[] args) throws IOException {
+        input();
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][0] = a[i + 1][0];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[0][j] = dp[0][j - 1] + a[1][j];
+            for (int i = 1; i < n; i++) {
+                dp[i][j] = Math.max(
+                    dp[i - 1][j] - (a[i][j] - (i < n - 1 ? a[i + 1][j] : 0)), 
+                    Math.max(dp[i - 1][j - 1], dp[i][j - 1]) + total[i][j]
+                );
+            }
+        }
+        System.out.println(dp[n - 1][m - 1]);
+    }
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] a = new int[n][m];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n][m];
         int[][] b = new int[n][m];
-        int[][] total = new int[n][m];
+        total = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -33,19 +54,5 @@ class Main {
                 total[i][j] = (i < n - 1 ? a[i + 1][j] : 0) + (i > 0 ? b[i - 1][j] : 0);
             }
         }
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n - 1; i++) {
-            dp[i][0] = a[i + 1][0];
-        }
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = dp[0][j - 1] + a[1][j];
-            for (int i = 1; i < n; i++) {
-                dp[i][j] = Math.max(
-                    dp[i - 1][j] - (a[i][j] - (i < n - 1 ? a[i + 1][j] : 0)), 
-                    Math.max(dp[i - 1][j - 1], dp[i][j - 1]) + total[i][j]
-                );
-            }
-        }
-        System.out.println(dp[n - 1][m - 1]);
     }
 }
