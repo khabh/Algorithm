e85fe794be2e57525ff0c275e8c44a088610549c
juha <84626225+khabh@users.noreply.github.com>
2024-11-16 17:52:15 +0900

    [Gold II] Title: 사과와 바나나, Time: 816 ms, Memory: 222976 KB -BaekjoonHub

--- 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
+++ 백준/Gold/3114. 사과와 바나나/사과와 바나나.java
@@ -11,33 +11,37 @@ class Main {
         int[][] a = new int[n][m];
         int[][] b = new int[n][m];
         int[][] total = new int[n][m];
-        int[][][] board = new int[n][m][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 String cur = st.nextToken();
-                board[i][j][cur.charAt(0) - 'A'] = Integer.parseInt(cur.substring(1));
+                int num = Integer.parseInt(cur.substring(1));
+                if (cur.charAt(0) == 'A') {
+                    a[i][j] = num;
+                } else {
+                    b[i][j] = num;
+                }
             }
         }
         for (int j = 0; j < m; j++) {
             for (int i = 1; i < n; i++) {
-                b[i][j] = b[i - 1][j] + board[i - 1][j][1];
+                b[i][j] += b[i - 1][j];
                 int ai = n - 1 - i;
-                a[ai][j] = a[ai + 1][j] + board[ai + 1][j][0];
+                a[ai][j] += a[ai + 1][j];
             }
             for (int i = 0; i < n; i++) {
-                total[i][j] = a[i][j] + b[i][j];
+                total[i][j] = (i < n - 1 ? a[i + 1][j] : 0) + (i > 0 ? b[i - 1][j] : 0);
             }
         }
         int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = a[i][0];
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][0] = a[i + 1][0];
         }
         for (int j = 1; j < m; j++) {
-            dp[0][j] = dp[0][j - 1] + a[0][j];
+            dp[0][j] = dp[0][j - 1] + a[1][j];
             for (int i = 1; i < n; i++) {
                 dp[i][j] = Math.max(
-                    dp[i - 1][j] - board[i][j][0], 
+                    dp[i - 1][j] - (a[i][j] - (i < n - 1 ? a[i + 1][j] : 0)), 
                     Math.max(dp[i - 1][j - 1], dp[i][j - 1]) + total[i][j]
                 );
             }
