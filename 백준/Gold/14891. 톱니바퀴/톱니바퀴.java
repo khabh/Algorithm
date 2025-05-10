b0c295f07eddc27a1b314c03e108bc6146622e76
juha <84626225+khabh@users.noreply.github.com>
2025-05-10 22:31:45 +0900

    [Gold V] Title: 톱니바퀴, Time: 112 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static char[][] board = new char[4][8];
+    static int[] up = new int[]{0, 0, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 4; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+        
+        int k = Integer.parseInt(br.readLine());
+        while (k-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int dir = Integer.parseInt(st.nextToken());
+            move(start, dir);
+        }
+        int result = 0;
+        int cur = 1;
+        for (int i = 0; i < 4; i++) {
+            if (board[i][up[i]] == '1') {
+                result += cur;
+            }
+            cur *= 2;
+        }
+        System.out.println(result);
+    }
+
+    private static void turn(int cur, int dir) {
+        up[cur] = (up[cur] - dir + 8) % 8;
+    }
+
+    private static void move(int start, int dir) {
+        int[] moves = new int[4];
+        moves[start] = dir;
+        for (int i = start - 1; i >= 0; i--) {
+            if (getRight(i) != getLeft(i + 1)) {
+                moves[i] = -moves[i + 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = start + 1; i < 4; i++) {
+            if (getLeft(i) != getRight(i - 1)) {
+                moves[i] = -moves[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            turn(i, moves[i]);
+        }
+    }
+
+    private static int getLeft(int i) {
+        return board[i][(up[i] - 2 + 8) % 8];
+    }
+
+    private static int getRight(int i) {
+        return board[i][(up[i] + 2) % 8];
+    }
+}
